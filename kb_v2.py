0dc68dcfa2db0823127c740e09fc4757112268cb
dasmikko <dasmikko@gmail.com>
2023-08-04 22:29:34 +0200

    Update kb_v2.py
    Fix left side mapping

--- kb_v2.py
+++ kb_v2.py
@@ -23,13 +23,18 @@ class KMKKeyboard(_KMKKeyboard):
     rgb_pixel_pin = pins[0]
 
     coord_mapping = []
-    coord_mapping.extend(ic(0, x, 8) for x in range(6))
-    coord_mapping.extend(ic(4, x, 8) for x in range(5, -1, -1))
-    coord_mapping.extend(ic(1, x, 8) for x in range(6))
-    coord_mapping.extend(ic(5, x, 8) for x in range(5, -1, -1))
-    coord_mapping.extend(ic(2, x, 8) for x in range(8))
-    coord_mapping.extend(ic(6, x, 8) for x in range(7, -1, -1))
-    coord_mapping.extend(ic(3, x, 8) for x in range(3, 8))
-    coord_mapping.extend(ic(7, x, 8) for x in range(7, 2, -1))
+    #coord_mapping.extend(ic(0, x, 8) for x in range(6))
+    #coord_mapping.extend(ic(4, x, 8) for x in range(5, -1, -1))
+    #coord_mapping.extend(ic(1, x, 8) for x in range(6))
+    #coord_mapping.extend(ic(5, x, 8) for x in range(5, -1, -1))
+    #coord_mapping.extend(ic(2, x, 8) for x in range(8))
+    #coord_mapping.extend(ic(6, x, 8) for x in range(7, -1, -1))
+    #coord_mapping.extend(ic(3, x, 8) for x in range(2, 7))
+    #coord_mapping.extend(ic(7, x, 8) for x in range(7, 2, -1))
     
-    print(coord_mapping)
+    coord_mapping = [
+        0,   1,  2,  3,  4,  5,                 37, 36, 35, 34, 33, 32, 
+        8,   9, 10, 11, 12, 13,                 45, 44, 43, 42, 41, 40, 
+        16, 17, 18, 19, 20, 21, 22, 23,         55, 54, 53, 52, 51, 50, 49, 48, 
+                    26, 28, 29, 30, 31,         63, 62, 61, 60, 59
+    ]
